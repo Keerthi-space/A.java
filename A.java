ca4104f1549babc0b321111e55275c6ac36c6f1d
Keerthi-space <keerthispace98@gmail.com>
2025-04-09 14:30:56 +0530

    Update A.java

--- A.java
+++ A.java
@@ -1,11 +1,12 @@
 package com.example; 
  
   public class A {     
-  static int s =  100;	
+  static int s =  100;
+	  int a=20;
   private void trial() {
 
 	  System.out.println("Hello Keerthi");
-	  dfgxcvbndfgh;
+	  
 }
   }
   
